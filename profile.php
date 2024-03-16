a6f12b0b07dae41af42ff4527c154941935809e4
Akraja712 <ashokkumar7hitter@gmail.com>
2024-03-16 16:59:42 +0530

    profile.php

--- profile.php
+++ profile.php
@@ -342,7 +342,7 @@ margin-left:40px;
                 <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
             </svg> copy
         </button>
-        <a href="https://ngcourse.nextgencareer.in/?refercode=<?php echo urlencode($refer_code); ?>" style="color:white;font-weight:bold; background-color:blue;border:2px solid black; border-radius:10px; font-size:17px; padding:10px; display: inline-block; text-decoration: none;">Refer friends</a>
+        <a href="https://ngcourse.nextgencareer.in/register.php?refercode=<?php echo urlencode($refer_code); ?>" style="color:white;font-weight:bold; background-color:blue;border:2px solid black; border-radius:10px; font-size:17px; padding:10px; display: inline-block; text-decoration: none;">Refer friends</a>
     </center>
 </div>
 
